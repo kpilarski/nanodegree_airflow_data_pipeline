fa7bee8b5aeb062e04b0a04e8e60db093f814ac1
kpilarski <konrad.pilarski@gmail.com>
2021-04-12 23:16:12 +0200

    feature: completing operators

--- plugins/operators/data_quality.py
+++ plugins/operators/data_quality.py
@@ -8,15 +8,25 @@ class DataQualityOperator(BaseOperator):
 
     @apply_defaults
     def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # conn_id = your-connection-name
+                 redshift_conn_id="",
+                 tables=[],
                  *args, **kwargs):
 
         super(DataQualityOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
+
+        self.redshift_conn_id = redshift_conn_id
+        self.tables = tables
 
     def execute(self, context):
-        self.log.info('DataQualityOperator not implemented yet')
+
+        redshift_hook = PostgresHook(self.redshift_conn_id)
+
+        for table in self.tables:
+            self.log.info(f"Checking Data Quality for table: {table}")
+            records = redshift_hook.get_records(f"SELECT COUNT(*) FROM {table}")
+            if len(records[0]) < 1 or len(records) < 1:
+                raise ValueError(f"Table {table} has no rows - Data Quality Check failed")
+            num_records = records[0][0]
+            if num_records < 1:
+                raise ValueError(f"Table {table} has 0 rows - Data Quality Check failed: ")
+            self.log.info(f"Data Quality check for {table} finished with success. Check passed with {records[0][0]} records")
--- plugins/operators/load_dimension.py
+++ plugins/operators/load_dimension.py
@@ -8,15 +8,34 @@ class LoadDimensionOperator(BaseOperator):
 
     @apply_defaults
     def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # conn_id = your-connection-name
+                 redshift_conn_id="",
+                 table="",
+                 sql_query="",
+                 skip_truncate=False,
                  *args, **kwargs):
 
         super(LoadDimensionOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
+
+        self.redshift_conn_id = redshift_conn_id
+        self.table = table
+        self.sql_query = sql_query
+        self.skip_truncate = skip_truncate
 
     def execute(self, context):
-        self.log.info('LoadDimensionOperator not implemented yet')
+        redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+        self.log.info(f"Loading dimension table {self.table}")
+        sql_code = ""
+
+        if self.skip_truncate:
+            sql_code = f"""
+                    BEGIN;
+                    INSERT INTO {self.table}
+                    {self.sql_query};"""
+        else:
+            sql_code = f"""
+                    BEGIN;
+                    TRUNCATE TABLE {self.table}; 
+                    INSERT INTO {self.table}
+                    {self.sql_query};"""
+
+        redshift.run(sql_code)
